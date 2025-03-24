ad0abae1297ea6f7689313471b16355ff4bd643e
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2025-03-24 17:10:13 +0200

    Bind interfaces to their respective repositories and set up policies for Project, Task, Comment, and Post in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,25 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ProjectInterface;
+use App\Repositories\ProjectRepository;
+use Illuminate\Support\Facades\Vite;
+use App\Interfaces\TaskInterface;
+use App\Interfaces\UserInterface;
+use App\Interfaces\PostInterface;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Project;
+use App\Models\Task;
+use App\Policies\CommentPolicy;
+use App\Policies\PostPolicy;
+use App\Policies\ProjectPolicy;
+use App\Policies\TaskPolicy;
+use App\Repositories\TaskRepository;
+use App\Repositories\UserRepository;
+use App\Repositories\PostRepository;
+use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +30,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ProjectInterface::class, ProjectRepository::class);
+        $this->app->bind(TaskInterface::class, TaskRepository::class);
+        $this->app->bind(UserInterface::class, UserRepository::class);
+        $this->app->bind(PostInterface::class, PostRepository::class);
     }
 
     /**
@@ -19,6 +41,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Vite::prefetch(concurrency: 3);
+
+        Gate::policy(Project::class, ProjectPolicy::class);
+        Gate::policy(Task::class, TaskPolicy::class);
+        Gate::policy(Comment::class, CommentPolicy::class);
+        Gate::policy(Post::class, PostPolicy::class);
     }
 }
