b5a21e0aa22a8bacbf78952af04f05377d678992
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2025-03-28 22:40:24 +0200

    changing the app logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,17 @@
-import AppLogoIcon from './app-logo-icon';
+import { FaProjectDiagram } from 'react-icons/fa';
 
 export default function AppLogo() {
     return (
         <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex size-10 items-center justify-center rounded-xl bg-green-300 text-lg dark:bg-green-500">
+                {/* <AppLogoIcon className="size-5 fill-current text-white dark:text-black" /> */}
+                <FaProjectDiagram className="fill-current text-white dark:text-white" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none">
+                    <span className="text-xl font-bold text-black dark:text-white">Plan</span>
+                    <span className="text-lg text-black dark:text-white">ova</span>
+                </span>
             </div>
         </>
     );
