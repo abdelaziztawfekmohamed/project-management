0cfc682b3b06ff7785cdc97d0ca6b5a174da0fec
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2025-04-17 14:47:17 +0200

    refactor: Update status interfaces and mappings for projects and tasks

--- resources/js/constants.ts
+++ resources/js/constants.ts
@@ -1,4 +1,12 @@
-export interface Status {
+export interface ProjectStatus {
+    todo: string;
+    in_progress: string;
+    in_review: string;
+    done: string;
+}
+
+export interface TaskStatus {
+    backlog: string;
     todo: string;
     in_progress: string;
     in_review: string;
@@ -11,28 +19,30 @@ export interface Priority {
     high: string;
 }
 
-export const PROJECT_STATUS_CLASS_MAP: Status = {
+export const PROJECT_STATUS_CLASS_MAP: ProjectStatus = {
     todo: 'bg-amber-500',
     in_progress: 'bg-blue-500',
     in_review: 'bg-slate-500',
     done: 'bg-green-500',
 };
 
-export const PROJECT_STATUS_TEXT_MAP: Status = {
+export const PROJECT_STATUS_TEXT_MAP: ProjectStatus = {
     todo: 'Todo',
     in_progress: 'In Progress',
     in_review: 'In Review',
     done: 'Done',
 };
 
-export const TASK_STATUS_CLASS_MAP: Status = {
+export const TASK_STATUS_CLASS_MAP: TaskStatus = {
+    backlog: 'bg-gray-600',
     todo: 'bg-amber-500',
     in_progress: 'bg-blue-500',
     in_review: 'bg-slate-500',
     done: 'bg-green-500',
 };
 
-export const TASK_STATUS_TEXT_MAP: Status = {
+export const TASK_STATUS_TEXT_MAP: TaskStatus = {
+    backlog: 'Backlog',
     todo: 'Todo',
     in_progress: 'In Progress',
     in_review: 'In Review',
